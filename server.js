f2f95ed38b1dad88a0078917a15c651d4c4d436b
Bruno Fernando Alves da Costa <40611225+bruunofernandz@users.noreply.github.com>
2019-04-06 19:33:34 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,6 +5,10 @@ app.get('/', (req, res) => {
     res.status(200).send({message: 'Ola mundo estou funcionando'})
 })
 
+app.get('/ola', (req, res) => {
+    res.status(200).send({message: 'Nova Rota ola'})
+})
+
 app.listen(3001, () => {
     console.log('API rodando na porta 3001')
 })
